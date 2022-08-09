77fd66acc3cf956e8e3ca935178f8205d8a1ba07
Timothy Loo <TimothyLoo07@gmail.com>
2022-08-08 19:48:03 -1000

    Complete Bare Minimum Requirements

--- exercises/bare_minimum/basicChaining.js
+++ exercises/bare_minimum/basicChaining.js
@@ -8,13 +8,45 @@
  * HINT: We exported some similar promise-returning functions in previous exercises
  */
 
-var fs = require('fs');
 var Promise = require('bluebird');
+var fs = Promise.promisifyAll(require('fs'));
+var request = require('needle');
 
 
 
 var fetchProfileAndWriteToFile = function(readFilePath, writeFilePath) {
   // TODO
+  return new Promise ((resolve, reject) =>{
+    fs.readFile(readFilePath, 'utf8', (err, file)=>{
+      if (err) {
+        reject(err);
+      } else {
+        let lines = file.split('\n');
+        resolve(lines[0]);
+      }
+    });
+  }).then((user) => {
+    return new Promise ((resolve, reject) => {
+      request.get('https://api.github.com/users/' + user, (err, response)=>{
+        if (err) {
+          reject(err);
+        } else {
+          let body = JSON.stringify(response.body);
+          resolve(body);
+        }
+      });
+    });
+  }).then((dataObj)=>{
+    return new Promise ((resolve, reject) => {
+      fs.writeFile(writeFilePath, dataObj, (err, res)=>{
+        if (err) {
+          reject(err);
+        } else {
+          resolve(res);
+        }
+      });
+    });
+  });
 };
 
 // Export these functions so we can test them
